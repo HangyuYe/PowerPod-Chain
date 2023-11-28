fbb27a02d28ac9f7efcfbb8c4a13b6d733149626
Jerry Ye <jerry.hangyu@gmail.com>
2023-11-28 22:09:25 +0800

    Update master.py

--- core/master.py
+++ core/master.py
@@ -1,14 +1,30 @@
 import time
 import threading
+import json
+from Crypto.Hash import keccak
 
 
 class Block:
-    def __init__(self, index, previous_hash, timestamp, data, hash):
+    def __init__(self, index, previous_hash, timestamp, data):
         self.index = index
         self.previous_hash = previous_hash
         self.timestamp = timestamp
         self.data = data
-        self.hash = hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        # 创建一个包含区块头数据的字典
+        header = {
+            'index': self.index,
+            'previous_hash': self.previous_hash,
+            'timestamp': self.timestamp,
+            'data': self.data,
+        }
+        # 将字典转换为字符串
+        header_string = json.dumps(header, sort_keys=True).encode()
+        # 计算字符串的哈希
+        return keccak.new(digest_bits=256, data=header_string).hexdigest()
+
 
     def __str__(self):
         return (f"Index: {self.index}\n"
@@ -25,7 +41,14 @@ class Blockchain:
 
     @staticmethod
     def create_genesis_block():
-        return Block(0, "0", time.time(), "Genesis Block", "somehash")
+        # Create the genesis block hash
+        # Use Int 0 as the hash for the genesis block
+        genesis_block_hash = keccak.new(digest_bits=256)
+        genesis_block_hash.update((0).to_bytes(1, byteorder="big"))
+        timestamp = int(time.time())
+        print("Genesis Block Hash:", genesis_block_hash.hexdigest())
+        block = Block(0, genesis_block_hash.hexdigest(), timestamp, "Genesis Block")
+        return block
 
     def get_latest_block(self):
         return self.chain[-1]
@@ -35,15 +58,14 @@ class Blockchain:
         self.chain.append(new_block)
 
     def schedule_block_addition(self):
-        threading.Timer(60, self.auto_add_block).start()  # 600 seconds = 10 minutes
+        threading.Timer(600, self.auto_add_block).start()  # every 10 seconds, add a new block
 
     def auto_add_block(self):
         index = len(self.chain)
         previous_hash = self.get_latest_block().hash
-        timestamp = time.time()
+        timestamp = int(time.time())
         data = f"Block {index} Data"
-        hash = f"somehash{index}"
-        new_block = Block(index, previous_hash, timestamp, data, hash)
+        new_block = Block(index, previous_hash, timestamp, data)
         self.add_block(new_block)
         print("New Block Added:")
         print(new_block)
